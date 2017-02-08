117e84fd056590a1f07f2563e8d186e09a9ec347
徐茁 <861265654@qq.com>
2017-02-08 13:49:49 +0800

    生成的代码里自动import相关类

--- blossom-compiler/src/main/java/blossom/compiler/TypeHolder.java
+++ blossom-compiler/src/main/java/blossom/compiler/TypeHolder.java
@@ -1,15 +1,13 @@
 package blossom.compiler;
 
 
+import com.squareup.javapoet.MethodSpec;
+
 import java.lang.annotation.Annotation;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import javax.lang.model.element.Element;
-import javax.lang.model.type.TypeMirror;
 
 import blossom.annotations.TieString;
 import blossom.annotations.TieView;
@@ -26,25 +24,20 @@ public class TypeHolder {
         holder.put(element, annoClass);
     }
 
-    public List<String> assembleStatements() {
+    public void addStatements(MethodSpec.Builder ctorBuilder) {
 
-        List<String> statements = new ArrayList<>();
         for (Element element : holder.keySet()) {
-            TypeMirror typeMirror = element.asType();
-            Element enclosingElement = element.getEnclosingElement();
 
             Class<? extends Annotation> annoClass = holder.get(element);
-            String statement = "";
+
             if (annoClass == TieString.class) {
                 int id = element.getAnnotation(TieString.class).value();
-                statement = String.format(Locale.getDefault(), "target.%s = res.getString(%d)", element.getSimpleName(), id);
+                ctorBuilder.addStatement("target.$L = res.getString($L)", element.getSimpleName(), id);
             } else if (annoClass == TieView.class) {
                 int id = element.getAnnotation(TieView.class).value();
-                statement = String.format(Locale.getDefault(), "target.%s = (%s) target.findViewById(%d)"
+                ctorBuilder.addStatement("target.$L = ($T) target.findViewById($L)"
                         , element.getSimpleName(), element.asType(), id);
             }
-            statements.add(statement);
         }
-        return statements;
     }
 }
